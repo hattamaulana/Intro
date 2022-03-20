0f5d6c0748f97aeebba487fab47e6dfbfc9c8481
hattamaulana <mahatta.maulana@gmail.com>
2022-03-20 12:56:49 +0700

    updates names & add projects

---
+++ ios_swift_ui/intro/introApp.swift
@@ -0,0 +1,17 @@
+//
+//  introApp.swift
+//  intro
+//
+//  Created by attaham on 20/03/22.
+//
+
+import SwiftUI
+
+@main
+struct introApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
