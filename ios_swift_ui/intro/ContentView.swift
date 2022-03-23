bbeb23593a1c1d28246c5b9a82c508214e7fcde2
hattamaulana <mahatta.maulana@gmail.com>
2022-03-23 20:04:02 +0700

    [ios_swiftui]: simple and fast

--- ios_swift_ui/intro/ContentView.swift
+++ ios_swift_ui/intro/ContentView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.white)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            HStack {
+                
+                VStack(alignment: .leading, spacing: 0, content: {
+                    Text("SNOWBOARD")
+                        .padding()
+                    
+                    Spacer(minLength: 300)
+                    
+                    Text("Riders\ntalked, and we listened")
+                        .font(.system(size: 40))
+                        .padding(.all)
+                    
+                    RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                        .frame(width: 14, height: 14)
+                        .padding()
+                    
+                    Text("Geometric")
+                        .font(.system(size: 40))
+                        .fontWeight(.heavy)
+                        .padding()
+                    
+                    Spacer()
+                    
+                    HStack {
+                        RoundedRectangle(cornerRadius: 65/2)
+                            .frame(width: 65, height: 65)
+                            .overlay(Image("ic_cart").frame(width: 65, height: 65))
+                        
+                        Text("View Collection")
+                    }
+                    .padding()
+                })
+                
+                Spacer()
+            
+                Image("skate")
+                    .resizable()
+                    .frame(width: 96, height: 700, alignment: .center)
+            }
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
